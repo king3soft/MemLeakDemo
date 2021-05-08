57b94244e326b7e8da3b5a229ce42cafa04b3ce3
yhnu <buutuud@gmail.com>
2021-05-08 20:39:01 +0800

    init

---
+++ MemLeakProject/Assets/Scenes/memLeakDemo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class memLeakDemo : MonoBehaviour
+{
+    List<int[]> m_HoldList = new List<int[]>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_HoldList.Add(new int[1024]);
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# UMemoryDemo_U3D
+Unity3D 内存工具演示_示例Demo
+
+com.king3soft.memLeak
